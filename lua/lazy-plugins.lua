3345840a9c78d924e3ad5e80e3ec615924752bfc
Michele Montesi <michelemontesi@brg.it>
2025-08-20 16:17:25 +0200

    remove nvim ide
    #HD-0000

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -62,7 +62,6 @@ require('lazy').setup({
   require 'kickstart.plugins.copilot',
   require 'kickstart.plugins.remotesshfs',
   require 'kickstart.plugins.lazy-git',
-  require 'kickstart.plugins.nvim-ide',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
